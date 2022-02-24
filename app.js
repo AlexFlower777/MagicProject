463f66c40ff5dd73d383f64e1c13078f6fb391ad
Inga Zhamsoeva <injoyme@gmail.com>
2022-02-24 11:36:05 +0300

    signup and login

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
-const express = require("express");
-const logger = require("morgan");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
-const FileStore = require("session-file-store")(session);
-const hbs = require("hbs");
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const hbs = require('hbs');
+
+const { addToLocals } = require('./middlewares/allMiddleware');
 
 const app = express();
 const PORT = 3000;
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "views"));
-app.use(logger("dev"));
-app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-hbs.registerPartials(path.join(__dirname, "views", "partials"));
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 
 app.use(
   session({
-    secret: "apojrglkdzfng;sakg", // строка для шифрования сессии
+    secret: 'apojrglkdzfng;sakg', // строка для шифрования сессии
     resave: false, // не пересохраняем сессию если не было изменений
     saveUninitialized: false, // не сохраняем сессию если она пустая
     cookie: { secure: false }, // не HTTPS
-    name: "userCookie", // имя сессионной куки
+    name: 'userCookie', // имя сессионной куки
     store: new FileStore(), // хранилище для куков - папка с файлами
-  })
+  }),
 );
 
-app.use("/", indexRouter);
-app.use("/users",usersRouter);
+app.use(addToLocals);
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 app.listen(PORT, () => {
   console.log(`server started PORT: ${PORT}`);
