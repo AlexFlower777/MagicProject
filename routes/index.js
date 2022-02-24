836bd0077222dbaf2c277fed7e4565f2467d7c10
Anton <Altair3464@yandex.ru>
2022-02-24 15:56:59 +0300

    хбска и ручка добавление новой карточки

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,13 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.get("/new", (req, res) => {
+  res.render("create");
+});
+
+router.post("/newImg", async (req, res) => {
+  console.log(req.body);
+  
+});
+
 module.exports = router;
