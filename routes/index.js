8023a43f93611c96cc6ec64234808f9d60f9f3e4
Inga Zhamsoeva <injoyme@gmail.com>
2022-02-24 19:42:50 +0300

    user router correction

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
-const router = require("express").Router();
-const { Card } = require("../db/models");
+const router = require('express').Router();
+const { Card } = require('../db/models');
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get('/', (req, res) => {
+  res.render('index');
 });
 
-
-router.get("/test", async (req, res) => {
+router.get('/test', async (req, res) => {
   const allCards = await Card.findAll({ raw: true });
   console.log(allCards);
-  res.render("index", { allCards });
+  res.render('index', { allCards });
 });
-
 
 module.exports = router;
